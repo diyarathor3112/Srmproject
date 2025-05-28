620cfd3d0437233e563fe1c9f15e0b02cc4b5e93
Nehakanwal121 <nehakanwal121@gmail.com>
2025-05-28 12:06:20 +0530

    app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 from monitor import get_system_stats
 from optimizer import suggest_optimizations
 from process_killer import list_processes, kill_process
+from optimizer import auto_kill_useless_processes
 
 app = Flask(__name__)
 
@@ -14,8 +15,15 @@ def index():
 
 @app.route('/kill/<int:pid>', methods=['POST'])
 def kill(pid):
-    success = kill_process(pid)
+    kill_process(pid)
     return redirect(url_for('index'))
+
+
+@app.route('/auto_optimize')
+def auto_optimize():
+    auto_kill_useless_processes()
+    return redirect('/')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
